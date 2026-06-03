51f17b0b2cb13d166e8ead47c875dff3d6c3bd7e
admin <23020803@vnu.edu.vn>
2026-06-03 21:11:31 +0700

    encoder

---
+++ ECU_Project/Core/App/Tang_Thuat_Toan/encoder.c
@@ -0,0 +1,6 @@
+#include "encoder.h"
+#include "app_bsp.h"
+
+// Cấp phát vật lý bộ đếm xung bánh xe
+int32_t enc_left_count = 0;
+int32_t enc_right_count = 0;
---
+++ ECU_Project/Core/App/Tang_Thuat_Toan/encoder.h
@@ -0,0 +1,4 @@
+#ifndef ENCODER_H_
+#define ENCODER_H_
+// cấu hình bộ lọc xung Encoder Tuần 2
+#endif
